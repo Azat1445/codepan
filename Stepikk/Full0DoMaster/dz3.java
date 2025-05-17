a396844b43189471eb2f28b5eaff6b9b583e47ce
Azat Ermolaev <azatermolaev2@gmail.com>
2025-05-18 01:30:44 +0300

    222

--- Stepikk/Full0DoMaster/dz3.java
+++ Stepikk/Full0DoMaster/dz3.java
@@ -2,14 +2,14 @@ package Full0DoMaster;
 
 public class dz3 {
     public static void main(String[] args) {
-//        int i1=5;
-//        int i2=11;
-//         double d1=5.5;
-//         double d2=1.3;
-//         long L=20l;
-//         double result1=0;
-//         result1=i2/d1+d2%i2-L;
-//         System.out.println(result1);
+        int i1=5;
+        int i2=11;
+         double d1=5.5;
+         double d2=1.3;
+         long L=20l;
+         double result1=0;
+         result1=i2/d1+d2%i2-L;
+         System.out.println(result1);
 
          int a=5;
          int b=8;
